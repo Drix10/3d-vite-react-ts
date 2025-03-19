67dd705d9ea62d824b2a6ed1ac1c67fc68eb1c6b
Aryan Menghare <87185902+aryanm830@users.noreply.github.com>
2025-03-19 19:12:32 +0000

    three.js added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import ThreeScene from './components/ThreeScene';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
       <p>Start prompting (or editing) with Coslynx.com :)</p>
+      <div className="w-[400px] h-[400px]">
+        <Canvas
+          camera={{ position: [0, 0, 5] }}
+          className="w-full h-full"
+        >
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+          <ThreeScene />
+        </Canvas>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
